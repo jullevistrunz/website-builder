a6d118600030bbf428e84c9f1ba4560a424e26c2
HeyPalu <j.l.strunz@gmail.com>
2023-04-26 19:13:20 +0200

    remove unneeded code

--- renderer.js
+++ renderer.js
@@ -663,26 +663,11 @@ function openViewSourceCode(view) {
 
 // encode html entities https://ourcodeworld.com/articles/read/188/encode-and-decode-html-entities-using-pure-javascript
 const htmlEntities = {
-  /**
-   * Converts a string to its html characters completely.
-   *
-   * @param {String} str String with unescaped HTML characters
-   **/
   encode: function (str) {
     var buf = []
     for (var i = str.length - 1; i >= 0; i--) {
       buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
     }
     return buf.join('')
-  },
-  /**
-   * Converts an html characterSet into its original character.
-   *
-   * @param {String} str htmlSet entities
-   **/
-  decode: function (str) {
-    return str.replace(/&#(\d+);/g, function (match, dec) {
-      return String.fromCharCode(dec)
-    })
   },
 }
