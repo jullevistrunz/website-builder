22ae4ae9aa8f7e26d28701cb38f471ac24dbc911
HeyPalu <j.l.strunz@gmail.com>
2023-05-11 18:16:22 +0200

    custom pages directory

--- preload.js
+++ preload.js
@@ -3,24 +3,24 @@ const childProcess = require('child_process')
 const fs = require('fs')
 const net = require('net')
 
-localStorage.setItem('settings', fs.readFileSync('settings.json'))
-localStorage.setItem('pages', JSON.stringify(fs.readdirSync('pages')))
+const settings = JSON.parse(fs.readFileSync('settings.json'))
+const pagePath = settings.collectionPage.pagesDirectoryPath
+localStorage.setItem('settings', JSON.stringify(settings))
+localStorage.setItem('pages', JSON.stringify(fs.readdirSync(pagePath)))
 
 contextBridge.exposeInMainWorld('preload', {
   test: () => console.info('test'),
   getPageInfo: (page) => {
-    return JSON.parse(fs.readFileSync(`pages/${page}/info.json`))
+    return JSON.parse(fs.readFileSync(`${pagePath}/${page}/info.json`))
   },
   getViews: (page) => {
-    return fs.readdirSync(`pages/${page}/views`)
+    return fs.readdirSync(`${pagePath}/${page}/views`)
   },
   getDirname: () => {
     return __dirname
   },
   getView: (page, view) => {
-    let file = fs
-      .readFileSync(`${__dirname}/pages/${page}/views/${view}`)
-      .toString()
+    let file = fs.readFileSync(`${pagePath}/${page}/views/${view}`).toString()
     file = file.slice(0, file.search('</body>'))
     return file.slice(file.search('<body>') + '<body>'.length)
   },
@@ -40,14 +40,14 @@ contextBridge.exposeInMainWorld('preload', {
         ).toString(16)
     )
     const port = Math.floor(Math.random() * 9000) + 1000
-    fs.mkdirSync(`pages/${id}`)
-    fs.mkdirSync(`pages/${id}/views`)
+    fs.mkdirSync(`${pagePath}/${id}`)
+    fs.mkdirSync(`${pagePath}/${id}/views`)
     fs.writeFileSync(
-      `pages/${id}/views/main.html`,
+      `${pagePath}/${id}/views/main.html`,
       '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Hello World</title></head><style>body{margin:0;}</style><body><h1>Hello World</h1></body></html>'
     )
     fs.writeFileSync(
-      `pages/${id}/info.json`,
+      `${pagePath}/${id}/info.json`,
       JSON.stringify({
         id: id,
         name: name,
@@ -57,23 +57,23 @@ contextBridge.exposeInMainWorld('preload', {
       })
     )
     fs.writeFileSync(
-      `pages/${id}/start.bat`,
+      `${pagePath}/${id}/start.bat`,
       `start "" "http://localhost:${port}/" 
       node "${__dirname}\\pages\\${id}\\index.js"`
     )
     fs.writeFileSync(
-      `pages/${id}/views/404.html`,
+      `${pagePath}/${id}/views/404.html`,
       '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>404 - Page Not Found</title></head><style>body{margin:0;}</style><body><div>404 - Page Not Found</div></body></html>'
     )
     fs.writeFileSync(
-      `pages/${id}/index.js`,
+      `${pagePath}/${id}/index.js`,
       `const port=${port},password="${password}",http=require("http"),fs=require("fs"),url=require("url"),server=http.createServer((function(req,res){const path=url.parse(req.url,!0).pathname;let query=url.parse(req.url,!0).query;if("/"==path)res.writeHead(200,{"Content-Type":"text/html"}),res.write(fs.readFileSync(__dirname+"/views/main.html")),res.end();else if("/exit"==path){if(query.p!=password)return res.writeHead(403,{"Content-Type":"text/html"}),res.write("403 - Forbidden"),void res.end();res.writeHead(200,{"Content-Type":"text/html"}),res.write("Good Bye"),res.end(),server.close()}else try{res.writeHead(200,{"Content-Type":"text/html"}),res.write(fs.readFileSync(__dirname+\`/views/\${path.substring("/".length)}.html\`)),res.end()}catch{res.writeHead(301,{Location:"/404"}),res.end()}}));server.listen(port,(function(error){error?console.log("Something went wrong"+error):console.log("Listening on port "+port)}));`
     )
   },
   startServerBat: (page) => {
     const ls = childProcess.spawn('cmd.exe', [
       '/c',
-      `pages\\${page}\\start.bat`,
+      `${pagePath}\\${page}\\start.bat`,
     ])
     ls.stdout.on('data', function (data) {
       console.info(page + ': stdout: ' + data)
@@ -99,7 +99,7 @@ contextBridge.exposeInMainWorld('preload', {
   },
   getRunningServers: () => {
     localStorage.setItem('runningServers', '[]')
-    fs.readdirSync('pages').forEach((page) => {
+    fs.readdirSync(pagePath).forEach((page) => {
       const server = net.createServer()
       server.once('error', function (err) {
         if (err.code === 'EADDRINUSE') {
@@ -115,11 +115,13 @@ contextBridge.exposeInMainWorld('preload', {
       server.once('listening', function () {
         server.close()
       })
-      server.listen(JSON.parse(fs.readFileSync(`pages/${page}/info.json`)).port)
+      server.listen(
+        JSON.parse(fs.readFileSync(`${pagePath}/${page}/info.json`)).port
+      )
     })
   },
   deletePage: (page) => {
-    fs.rmSync(`pages/${page}`, { recursive: true, force: true })
+    fs.rmSync(`${pagePath}/${page}`, { recursive: true, force: true })
   },
   getPlugins: () => {
     return fs.readdirSync('plugins')
